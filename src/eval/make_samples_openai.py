e0b47181ed9ad45aa65fd7482f49f548c5aea346
mariodeaconescu <mariodeaconescu2003@gmail.com>
2025-04-30 16:05:44 +0300

    Fix incorrect wall dimensions in ImagePlan creation.
    Previously, the wall dimensions in ImagePlan were being returned incorrectly. This change adjusts the calculation to ensure the walls are correctly processed by applying the proper transformation (`walls * 2 - 1`).

---
+++ src/eval/make_samples_openai.py
@@ -0,0 +1,52 @@
+import os
+from typing import Optional
+
+import torch
+from tqdm.contrib.concurrent import process_map
+
+from src.eval.make_eval_gt import save_img_plan
+from src.gaussian_noise import BetaSchedule, ModelMeanType, ModelVarType
+from src.respace import SpacedDiffusion, space_timesteps
+from src.rplan.dataset import RPlanImageDataset
+from src.rplan.types import ImagePlan
+from src.rplan_masks.sample_unet_bubbles import sample_plans_bubbles
+
+
+def make_samples_bubbles(model, output_path: str, num_samples: int = 1000, batch_size: int = 1,
+                               num_timesteps: int = 1000,
+                               data_path='../data/rplan', mask_size=64, condition_scale=1, rescaled_phi=0, ddim=True,
+                               thin_walls=True, simplify: Optional[float] = None, target_size: Optional[tuple[int, int]] = None,
+                               num_workers: int = 8):
+    T = 1000
+    device = torch.device(
+        'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
+
+    diffusion = SpacedDiffusion(space_timesteps(T, f'ddim{num_timesteps}'), T, BetaSchedule.COSINE,
+                                ModelMeanType.EPSILON,
+                                ModelVarType.FIXED_SMALL,
+                                device=device)
+
+    generated = 0
+    epoch = 0
+
+    os.makedirs(output_path, exist_ok=True)
+    dataset = RPlanImageDataset(data_path=data_path, mask_size=(mask_size, mask_size), shuffle_rooms=True,
+                                random_scale=0.6)
+    while generated < num_samples:
+        if condition_scale > 0:
+            raise NotImplementedError("Conditioning on is not implemented yet.")
+        else:
+            bubbles = None
+        samples = sample_plans_bubbles(diffusion, model, num_samples=batch_size, device=device, data_path=data_path,
+                                          mask_size=mask_size, bubbles=bubbles,
+                                          condition_scale=condition_scale, rescaled_phi=rescaled_phi, ddim=ddim)
+
+        plans = [ImagePlan(walls=walls, image=rooms, door_image=doors) for rooms, walls, doors in samples]
+
+        plans = [plan.to_plan(thin_walls=thin_walls, simplify=simplify, target_size=target_size)[1] for plan in plans]
+
+        process_map(save_img_plan, [output_path] * len(plans), plans, range(len(plans)), [epoch] * len(plans),
+                    max_workers=num_workers, chunksize=100, desc=f"Generating Step {epoch}: {generated}/{num_samples}",
+                    total=len(plans))
+        generated += len(plans)
+        epoch += 1
---
+++ src/rplan_masks/sample_unet_bubbles.py
@@ -0,0 +1,59 @@
+from functools import partial
+from typing import Optional
+
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+
+from src.gaussian_noise import GaussianDiffusion, BetaSchedule, ModelMeanType, ModelVarType
+from src.rplan.dataset import RPlanMasksDataset, RPlanImageDataset
+from src.rplan.types import MaskPlan, RoomType, ImagePlan
+from src.rplan_masks.karras.cfg import CFGUnet
+from src.rplan_masks.karras.denoise import GithubUnet
+
+
+@torch.no_grad()
+def sample_plans_bubbles(diffusion: GaussianDiffusion, model, num_samples: int = 1, data_path='data/rplan', mask_size: int = 64,
+                bubbles: Optional[np.ndarray] = None, condition_scale: float = 1.0,
+                rescaled_phi: float = 0.0, ddim: bool = False,
+                device: torch.device = torch.device('cpu')):
+    dataset = RPlanImageDataset(data_path=data_path, mask_size=(mask_size, mask_size), shuffle_rooms=True,
+                                random_scale=0.6)
+    random_samples = [dataset[np.random.randint(0, len(dataset))] for _ in range(num_samples)]
+    input_bubbles = bubbles
+    if bubbles is not None:
+        for i in range(len(random_samples)):
+            random_samples[i].bubbles = bubbles[i]
+    # random_sample = dataset[0]
+    # random_sample = from_export('notebooks/data/export')
+    # with open('data/rplan/1.json') as f:
+    #     raw_plan = RawPlan(json.load(f))
+    #
+    # plan = Plan.from_raw(raw_plan)
+    # random_sample = TorchTransformerPlan.from_plan(plan, 32, 100, front_door_at_end=True)
+    random_sample_batched = ImagePlan.collate(random_samples)
+
+    images, walls, doors, _, bubbles, masks = random_sample_batched
+    images, walls, doors, bubbles, masks = images.to(device), walls.to(device), doors.to(device), bubbles.to(
+        device), masks.to(device)
+    # room_types = room_types.float()
+    # real_room_types = [RoomType.from_one_hot(room_type) for room_type in room_types[0]]
+    masks = masks.float()
+    # masks = torch.ones_like(masks, device=device)
+    x = torch.cat([images, walls, doors], dim=1)
+
+    model_kwargs = {
+        'masks': masks,
+        'bubbles': bubbles if input_bubbles is not None else None,
+        'cond_scale': condition_scale,
+        'rescaled_phi': rescaled_phi,
+        # 'src_key_padding_mask': src_key_padding_mask,
+        # 'nodes_to_graph': nodes_to_graph,
+    }
+
+    if ddim:
+        samples, _ = diffusion.ddim_sample_loop(model, x.shape, model_kwargs=model_kwargs)
+    else:
+        samples, _ = diffusion.p_sample_loop(model, x.shape, model_kwargs=model_kwargs)
+    samples = samples.cpu().numpy()
+    return samples
